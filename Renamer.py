dc07f25d53886e55fa371e30225be1e3c97d9559
PoliKo13 <55900445+PoliKo13@users.noreply.github.com>
2020-04-25 11:19:14 +0300

    Add files via upload

---
+++ Renamer.py
@@ -0,0 +1,27 @@
+import os
+
+#os.rename('index.jpeg', 'cat.jpeg')
+
+
+path = 'memes/'
+time = {}
+newname = ['a', 'b', 'c', 'd', 'e', 'f']
+for filename in os.listdir(path):
+    if filename.endswith('mp3'):
+        info = os.stat(path + filename)
+        print(filename , info.st_ctime)
+        time.update({info.st_ctime : filename})
+print(time)
+time_list = list(time.keys())
+print(time_list)
+i = 0
+while len(time)>0:
+    print(min(time_list))
+    early = time_list.pop(time_list.index(min(time_list)))
+    name = time.pop(early)
+    print(name)
+    os.rename(path + name, path + newname[i] + '.mp3')
+    i = i + 1
+
+        
+    
